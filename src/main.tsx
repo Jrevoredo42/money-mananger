948b4e3001fc6fa25b2fd28025a88ccaea3ea7bb
Jrevoredo42 <revoredojf@gmail.com>
2022-05-19 20:30:29 -0300

    foi adicionado a lib para criacao de dados estaticos miragejs, juntamente do servidor com os dados no main e a chamada desses dados no componente de transacoes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createServer } from 'miragejs';
 import { App } from './App'
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+
+
+    this.get('/transactions', () => {
+      return [
+        {
+
+          id: 1,
+          title: 'first transaction',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createAt: new Date()
+        }
+      ]
+    })
+
+  }
+})
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
