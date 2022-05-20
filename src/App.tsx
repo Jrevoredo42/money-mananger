8bea434d51a4dd7ae427bf52a64c3f2f202a24c1
Jrevoredo42 <revoredojf@gmail.com>
2022-05-20 12:37:42 -0300

    foi criado modal com suas configurações no app e no index do header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import { GlobalStyle } from './styles/global';
 import { Header } from '../src/components/Header'
 import { Dashboard } from './components/Dashboard';
+import { useState } from 'react';
+import  Modal  from 'react-modal';
 
+Modal.setAppElement('#root');
 
 export function App () {
+
+const [isNewTansactionOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModalOpen(true);
+
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModalOpen(false);
+    }
+
+
   return (
        <>
-      <Header /> 
-      <Dashboard/> 
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} /> 
+      <Dashboard/>
+
+      <Modal isOpen={isNewTansactionOpen}
+             onRequestClose={handleCloseNewTransactionModal}>
+              <h2>Cadastrar Transação </h2>
+      </Modal>
     <GlobalStyle/>
         </>
   )
